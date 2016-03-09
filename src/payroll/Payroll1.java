7e7fe6c324fc49538bfc6c6e0a076540624d328b
Adel Syafrizal Pratama <ngekngok101@gmail.com>
2016-03-09 13:52:43 +0700

    Pegawai

---
+++ src/payroll/Payroll1.java
@@ -0,0 +1,41 @@
+package payroll;
+
+public class Payroll1 {
+
+    public static void main(String[] args) {
+        Kerja p = new Kerja();
+        p.setKaryawan();
+        p.setAbsensi();
+        p.getBonus();
+        p.getDenda();
+        p.getTotal();
+        p.display();
+        
+        Kerja p1 = new Kerja();
+        p1.setKaryawan();
+        p1.setAbsensi();
+        p1.getBonus();
+        p1.getDenda();
+        p1.getTotal();
+        p1.display();
+        System.out.println("");
+        
+        Kerja p2 = new Kerja();
+        p2.setKaryawan();
+        p2.setAbsensi();
+        p2.getBonus();
+        p2.getDenda();
+        p2.getTotal();
+        p2.display();
+        System.out.println("");
+        
+        Kerja p3 = new Kerja();
+        p3.setKaryawan();
+        p3.setAbsensi();
+        p3.getBonus();
+        p3.getDenda();
+        p3.getTotal();
+        p3.display();
+
+    }
+}
